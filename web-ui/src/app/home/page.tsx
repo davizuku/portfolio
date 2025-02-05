8bd194c2d3e554719484cf1bc7777a1b808502a0
David Álvarez Pons <david_alvarezpons@hotmail.com>
2025-02-05 18:13:01 +0100

    Add fixed navbar upon scrolling

--- web-ui/src/app/home/page.tsx
+++ web-ui/src/app/home/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FaLinkedin, FaGithub, FaCode } from "react-icons/fa"; // Import icons
@@ -7,55 +9,76 @@ import PortfolioSection from "../components/ui/portfolioSection";
 
 export default function Home() {
 
+    const profileImage = <Image src="/img/profile.jpg"
+        alt="David Álvarez Pons"
+        className="rounded-full"
+        sizes="100%"
+        width={100}
+        height={100}
+        style={{ width: "100%", height: "auto" }}
+    />
+    const contactLinks = [
+        <Link
+            key="linkedin"
+            href="https://www.linkedin.com/in/david-alvarez-pons/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white"
+        >
+            <FaLinkedin />
+        </Link>,
+        <Link
+            key="github"
+            href="https://github.com/davizuku"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white"
+        >
+            <FaGithub/>
+        </Link>,
+        <Link
+            key="leetcode"
+            href="https://leetcode.com/davizuku"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white"
+        >
+            <FaCode />
+        </Link>,
+    ];
+
+    const bioSection = useRef(null);
+    const [showHeader, setShowHeader] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const section = bioSection.current as HTMLElement | null;
+            if (section) {
+                setShowHeader(window.scrollY >= section.clientHeight);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    });
 
     return (
-        <div className="min-h-screen flex flex-col items-center">
-            <header className="hidden w-full max-w-4xl flex justify-between items-center py-4">
-                <h1 className="text-3xl font-bold">David Álvarez Pons</h1>
+        <div className="min-h-screen flex flex-col items-center overflow-hidden">
+            <header className={`${showHeader ? '' : 'hidden'} fixed bg-accent z-10 top-0 min-w-full flex justify-between items-center p-2 drop-shadow-2xl`}>
+                <div className="rounded-full justify-center w-10 md:w-12"><Link href="#">{profileImage}</Link></div>
+                <h1 className="text-center text-xl md:text-3xl font-bold">David Álvarez Pons</h1>
+                <div className="flex space-x-4 text-sm md:text-xl text-primary">{contactLinks}</div>
             </header>
 
             <main className="w-full flex flex-col">
-                <section id="bio" className="w-full h-screen flex flex-col justify-center bg-secondary">
+                <section ref={bioSection} className="w-full h-screen flex flex-col justify-center bg-secondary">
                     <div className="flex flex-col items-center justify-center gap-5">
-                        <div className="rounded-full justify-center w-2/3 md:w-1/2 lg:w-1/3 relative">
-                            <Image src="/img/profile.jpg" alt="David Álvarez Pons"
-                                className="rounded-full"
-                                sizes="100%"
-                                width={100}
-                                height={100}
-                                style={{ width: "100%", height: "auto" }}
-                            />
-                        </div>
+                        <div className="rounded-full justify-center w-2/3 md:w-1/2 lg:w-1/3 relative">{profileImage}</div>
                         <h1 className="text-3xl md:text-4xl text-primary font-bold">David Álvarez Pons</h1>
                         <p className="text-primary text-center italic m-auto w-2/3 md:w-1/2 text-balance">
                             Versatile Software Engineer with a passion for sustainable, scalable solutions and team growth.
                         </p>
-                        <div className="flex space-x-4 text-primary">
-                            <Link
-                                href="https://www.linkedin.com/in/david-alvarez-pons/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="hover:text-white"
-                            >
-                                <FaLinkedin size={24} />
-                            </Link>
-                            <Link
-                                href="https://github.com/davizuku"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="hover:text-white"
-                            >
-                                <FaGithub size={24} />
-                            </Link>
-                            <Link
-                                href="https://leetcode.com/davizuku"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="hover:text-white"
-                            >
-                                <FaCode size={24} />
-                            </Link>
-                        </div>
+                        <div className="flex space-x-4 text-2xl text-primary">{contactLinks}</div>
                     </div>
                     <div className="absolute bottom-10 w-full text-center animate-bounce opacity-50 text-xs md:text-base">
                         <p className="text-primary">Scroll down to learn more</p>
