f53e573d7fbf51a00a53920fc499d215986f8700
David Álvarez Pons <david_alvarezpons@hotmail.com>
2025-02-17 20:15:38 +0100

    Switch to paid models

--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -27,18 +27,24 @@ export async function POST(req: NextRequest) {
     // TODO: add fallback to paid model when rate limit reached
     model = "google/gemini-2.0-flash-001"; // Pay 0.1 -> 0.4
     model = 'sophosympatheia/rogue-rose-103b-v0.2:free';
-    model = "deepseek/deepseek-r1-distill-llama-8b"; // Pay 0.04 -> 0.04
     model = "deepseek/deepseek-r1-distill-llama-70b:free"
-    model = "mistralai/mistral-nemo"
-    model = "mistralai/ministral-8b"
+    model = "deepseek/deepseek-r1-distill-llama-8b"; // Pay 0.04 -> 0.04
+    model = "mistralai/mistral-nemo"  // Pay 0.035 -> 0.08
     model = "google/gemini-2.0-flash-lite-preview-02-05:free";
+    model = "mistralai/ministral-8b" // Pay 0.1 -> 0.1
+    model = "meta-llama/llama-3.2-1b-instruct" // Pay 0.01 -> 0.01
+    model = "google/gemini-flash-1.5-8b" // Pay 0.075 -> 0.3
 
     // @see: https://sdk.vercel.ai/docs/reference/ai-sdk-core/stream-text
     const result = streamText({
       model: openrouter(model),
       system: systemPrompt,
       temperature: 0.5,
+      maxTokens: 1000,
       messages: messages,
+      onError: ({ error }) => {
+        console.error(`An error occurred while generating text in api/agent for prompt: '${JSON.stringify(messages)}': ${error}`);
+      }
     });
     return result.toDataStreamResponse();
   } catch (error: any) {
