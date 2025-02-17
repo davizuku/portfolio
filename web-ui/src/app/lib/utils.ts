0db71e8f486938486f86e7a7b11822ce58e202c6
David Álvarez Pons <david_alvarezpons@hotmail.com>
2025-02-17 10:06:12 +0100

    Remove auxiliary function to deal with streams

--- web-ui/src/app/lib/utils.ts
+++ web-ui/src/app/lib/utils.ts
@@ -4,21 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// @ref https://github.com/jherr/deepseek-start
-export function streamAsyncIterator(reader: ReadableStreamDefaultReader<Uint8Array>) {
-  const decoder = new TextDecoder("utf-8");
-  return {
-    async *[Symbol.asyncIterator]() {
-      try {
-        while (true) {
-          const { done, value } = await reader.read();
-          if (done) return;
-          yield decoder.decode(value);
-        }
-      } finally {
-        reader.releaseLock();
-      }
-    },
-  };
-}
