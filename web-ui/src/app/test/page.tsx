0c3ec0c70b7ca2ae15d8d4604e0eb93961fbc8db
David Álvarez Pons <david_alvarezpons@hotmail.com>
2025-02-17 10:06:25 +0100

    Simplify test page

--- web-ui/src/app/test/page.tsx
+++ web-ui/src/app/test/page.tsx
@@ -1,29 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { streamAsyncIterator } from "@/app/lib/utils";
-
 export default function Page() {
-  const [buffer, setBuffer] = useState<string>("");
-  useEffect( () => {
-    const fetchStream = async () => {
-      const response = await fetch('api/agent', {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      if (!response.body) return;
-      const reader = response.body.getReader();
-      for await (const value of streamAsyncIterator(reader)) {
-        const { text } = JSON.parse(value);
-        setBuffer((prev) => prev + text);
-      }
-    }
-    fetchStream().catch(console.error);
-    return () => {};
-  }, []);
-
-  return (<pre>Buffer: {buffer}</pre>
+  return (<pre>This is a test</pre>
   );
 }
