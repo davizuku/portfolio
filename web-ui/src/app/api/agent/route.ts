bd04ea12c4b5337082e5c1235c1736e3df885a64
David Álvarez Pons <david_alvarezpons@hotmail.com>
2025-02-17 13:00:35 +0100

    Choose model

--- web-ui/src/app/api/agent/route.ts
+++ web-ui/src/app/api/agent/route.ts
@@ -26,16 +26,18 @@ export async function POST(req: NextRequest) {
     let model = "";
     // TODO: choose model or create fallback logic
     model = "google/gemini-2.0-flash-001"; // Pay 0.1 -> 0.4
-    model = "google/gemini-2.0-flash-lite-preview-02-05:free";
     model = 'sophosympatheia/rogue-rose-103b-v0.2:free';
     model = "deepseek/deepseek-r1-distill-llama-8b"; // Pay 0.04 -> 0.04
     model = "deepseek/deepseek-r1-distill-llama-70b:free"
+    model = "mistralai/mistral-nemo"
+    model = "mistralai/ministral-8b"
+    model = "google/gemini-2.0-flash-lite-preview-02-05:free";
 
+    // @see: https://sdk.vercel.ai/docs/reference/ai-sdk-core/stream-text
     const result = streamText({
       model: openrouter(model),
       system: systemPrompt,
       temperature: 0.5,
-      maxTokens: 300,
       messages: messages,
     });
     return result.toDataStreamResponse();
