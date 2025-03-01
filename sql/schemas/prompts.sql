82d0f464a7aef1132091d9b307b34124d27f65f2
David Álvarez Pons <david_alvarezpons@hotmail.com>
2025-03-01 12:42:44 +0100

    Add trigger to keep updated_at field up to date

--- sql/schemas/prompts.sql
+++ sql/schemas/prompts.sql
@@ -7,3 +7,17 @@ CREATE TABLE IF NOT EXISTS prompts (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
 );
+
+-- This does not work with ./scripts/execute-sql.js
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS
+$$ BEGIN
+NEW.updated_at = now();
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS update_modified_time ON prompts;
+CREATE TRIGGER update_modified_time
+BEFORE UPDATE ON prompts
+FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
